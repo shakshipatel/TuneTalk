76a3c9044f2df1f3fa3094a9eb28b222d79c58af
Shakshi Patel <shakship003@gmail.com>
2025-07-29 19:54:24 +0530

    Frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
+import AlbumPage from "./pages/album/AlbumPage";
+import AdminPage from "./pages/admin/AdminPage";
+
+import { Toaster } from "react-hot-toast";
+import NotFoundPage from "./pages/404/NotFoundPage";
+
+function App() {
+	return (
+		<>
+			<div>my name is shakshi</div>
+		</>
+	);
+}
+
+export default App;
