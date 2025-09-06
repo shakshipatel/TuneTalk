cdeab59f19d251fd6a945f5b4387d1f5587f4cc7
Shakshi Patel <shakship003@gmail.com>
2025-09-07 00:51:46 +0530

    fixes

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.VITE_BACKEND_URL || "https://tunetalk-422p.onrender.com",
+	baseURL: import.meta.env.VITE_BACKEND_URL || "https://tunetalk-422p.onrender.com/api",
 });
