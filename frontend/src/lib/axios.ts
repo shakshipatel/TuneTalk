e981b77ac999653b70d23c8f5f15b49f3f7b4fdf
Shakshi Patel <121892732+shakshipatel@users.noreply.github.com>
2025-09-07 00:34:57 +0530

    Update axios.ts

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api",
+	baseURL: import.meta.env.VITE_BACKEND_URL || "https://tune-talk-4id4.vercel.app",
 });
