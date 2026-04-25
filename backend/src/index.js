101b75ecd8538bda527e033da2a4b3e4b13ebac8
prashant-software-developer <prashantkumarsingh.work@gmail.com>
2026-04-26 03:34:26 +0530

    CORS Fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,44 +28,45 @@ const httpServer = createServer(app);
 initializeSocket(httpServer);
 
 app.use(
-	cors({
-		origin: [
-			"http://localhost:3000",
-			"http://localhost:5173",
-			"https://tunetalk.shakshipatel.in",
-			process.env.CORS_ORIGIN,
-		].filter(Boolean),
-		credentials: true,
-	})
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://tunetalk.shakshipatel.in",
+      "https://www.tunetalk.shakshipatel.in",
+      process.env.CORS_ORIGIN,
+    ].filter(Boolean),
+    credentials: true,
+  }),
 );
 
 app.use(express.json()); // to parse req.body
 app.use(clerkMiddleware()); // this will add auth to req obj => req.auth
 app.use(
-	fileUpload({
-		useTempFiles: true,
-		tempFileDir: path.join(__dirname, "tmp"),
-		createParentPath: true,
-		limits: {
-			fileSize: 10 * 1024 * 1024, // 10MB  max file size
-		},
-	})
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024, // 10MB  max file size
+    },
+  }),
 );
 
 // cron jobs
 const tempDir = path.join(process.cwd(), "tmp");
 cron.schedule("0 * * * *", () => {
-	if (fs.existsSync(tempDir)) {
-		fs.readdir(tempDir, (err, files) => {
-			if (err) {
-				console.log("error", err);
-				return;
-			}
-			for (const file of files) {
-				fs.unlink(path.join(tempDir, file), (err) => {});
-			}
-		});
-	}
+  if (fs.existsSync(tempDir)) {
+    fs.readdir(tempDir, (err, files) => {
+      if (err) {
+        console.log("error", err);
+        return;
+      }
+      for (const file of files) {
+        fs.unlink(path.join(tempDir, file), (err) => {});
+      }
+    });
+  }
 });
 
 app.use("/api/users", userRoutes);
@@ -76,18 +77,23 @@ app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-	});
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 // error handler
 app.use((err, req, res, next) => {
-	res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message });
+  res.status(500).json({
+    message:
+      process.env.NODE_ENV === "production"
+        ? "Internal server error"
+        : err.message,
+  });
 });
 
 httpServer.listen(PORT, () => {
-	console.log("Server is running on port " + PORT);
-	connectDB();
+  console.log("Server is running on port " + PORT);
+  connectDB();
 });
